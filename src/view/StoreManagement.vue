3c52c443f1524fb98f9d70aca7b0d4e017aa82f4
warmtimer <18351922593@163.com>
2019-01-02 17:33:02 +0800

    修改会员办理页面代码格式

--- src/view/StoreManagement.vue
+++ src/view/StoreManagement.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>StoreManagement</div>
+  <div>
+    <el-card shadow="hover">
+      <div slot="header">
+        <span>门店信息</span>
+      </div>
+      <el-row>
+        <el-col :span="15">
+          <el-form :model="storeForm" label-width="100px">
+            <el-form-item label="门店名称：" prop="storeName">
+              <el-input v-model="storeForm.storeName"></el-input>
+            </el-form-item>
+            <el-form-item label="门店地址：" prop="storeAddress">
+              <el-input v-model="storeForm.storeAddress"></el-input>
+            </el-form-item>
+            <el-form-item label="营业时间：" prop="businessHour">
+              <el-time-picker
+                is-range
+                v-model="storeForm.businessHour"
+                range-separator="~"
+                start-placeholder="开始时间"
+                end-placeholder="结束时间"
+                placeholder="请选择时间">
+              </el-time-picker>
+            </el-form-item>
+            <el-form-item label="客服电话：" prop="servicePhone">
+              <el-input v-model="storeForm.servicePhone"></el-input>
+            </el-form-item>
+          </el-form>
+          <el-button type="primary">确认上传</el-button>
+        </el-col>
+        <el-col :span="7" :offset="2">
+          <p>上传图片：</p>
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            multiple
+            list-type="picture-card"
+            :before-upload="beforeUploadPicture"
+            :on-preview="handlePictureCardPreview"
+            :on-progress="uploadProgress"
+            :on-remove="handleRemove"
+            :on-success="uploadSuccess"
+            :on-error="uploadError"
+            :file-list="fileList"
+            class="imgWidth imgListWidth">
+            <i class="el-icon-plus icon-position"></i>
+          </el-upload>
+          <el-dialog :visible.sync="imgVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,16 +59,74 @@
     name: 'StoreManagement',
     data() {
       return {
-        msg: ''
+        storeForm: {
+          storeName: '',
+          storeAddress: '',
+          businessHour: '',
+          servicePhone: '',
+        },
+        dialogImageUrl: '',
+        imgVisible: false,
+        fileList: [{
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }, {
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }, {
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }, {
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }, {
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+        }]
       }
     },
-    methods: {},
+    methods: {
+      beforeUploadPicture() {
+        console.log('beforeUploadPicture')
+      },
+      handlePictureCardPreview(file) {
+        console.log(file)
+      },
+      uploadProgress() {
+        console.log('uploadProgress')
+      },
+      handleRemove() {
+        console.log('handleRemove')
+      },
+      uploadSuccess() {
+        console.log('uploadSuccess')
+      },
+      uploadError() {
+        console.log('uploadError')
+      }
+    },
     mounted: function () {
 
     }
   }
 </script>
 
-<style lang="less">
-
+<style lang="less" scoped>
+  .imgWidth /deep/ .el-upload--picture-card {
+    width: 60px;
+    height: 60px;
+    position: relative;
+  }
+  .imgListWidth /deep/ .el-upload-list--picture-card .el-upload-list__item {
+    display: inline-block;
+    width: 60px;
+    height: 60px;
+  }
+  .icon-position {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 20px;
+    transform: translate(-50%,-50%);
+  }
 </style>
