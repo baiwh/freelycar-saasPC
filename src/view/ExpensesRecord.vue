c6e1e949026e8cdeed6748aa50761b0b3d099d0c
warmtimer <18351922593@163.com>
2019-01-03 11:55:07 +0800

    新增消费记录组件代码

--- src/view/ExpensesRecord.vue
+++ src/view/ExpensesRecord.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>ExpensesRecord</div>
+  <div>
+    <el-row>
+      <el-radio-group v-model="tabPosition" @change="onTabChange">
+        <el-radio-button label="today">今日</el-radio-button>
+        <el-radio-button label="thisMonth">本月</el-radio-button>
+        <el-radio-button label="search">全部</el-radio-button>
+      </el-radio-group>
+    </el-row>
+    <el-card shadow="hover">
+      <div slot="header">
+        <p>合计消费</p>
+        <el-row v-if="visible">
+          <span>查找日期：</span>
+          <el-date-picker
+            v-model="datePickerValue"
+            type="daterange"
+            range-separator="~"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            class="dateWidth">
+          </el-date-picker>
+        </el-row>
+      </div>
+      <el-table :data="expensesRecordTable">
+        <el-table-column type="index" label="序号" align="center"/>
+        <el-table-column property="projectName" label="项目" align="center"/>
+        <el-table-column property="expensesAmounts" label="消费金额" align="center"/>
+        <el-table-column property="payMethods" label="支付方式" align="center"/>
+        <el-table-column property="isUseCard" label="是否使用会员卡扣" align="center"/>
+        <el-table-column property="serviceTime" label="服务时间" align="center"/>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,16 +39,28 @@
     name: 'ExpensesRecord',
     data() {
       return {
-        msg: ''
+        tabPosition: 'today',
+        visible: false,
+        datePickerValue: '',
+        expensesRecordTable: []
       }
     },
-    methods: {},
+    methods: {
+      onTabChange(e) {
+        this.visible = e === 'search' ? true : false
+      }
+    },
     mounted: function () {
 
     }
   }
 </script>
 
-<style lang="less">
-
+<style lang="less" scoped>
+  .el-card {
+    margin-top: 40px;
+  }
+  .dateWidth {
+    width: 70%;
+  }
 </style>
