c7ec1331d35844db9e1c3cfdee304175f0e3bb56
baiwh <471974983@qq.com>
2019-01-08 17:36:21 +0800

    增加两个页面

---
+++ src/view/ProcurementExpenditure.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>ProcurementExpenditure</div>
+</template>
+
+<script>
+  export default {
+    name: 'ProcurementExpenditure',
+    data() {
+      return {
+        msg: ''
+      }
+    },
+    methods: {},
+    mounted: function () {
+
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>home</div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        msg: ''
+      }
+    },
+    methods: {},
+    mounted: function () {
+
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
