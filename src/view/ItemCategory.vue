1cb7323125b1b8a557fe9a85558edb28e9d1e865
baiwh <471974983@qq.com>
2019-01-30 09:29:28 +0800

    批量删除项目管理

--- src/view/ItemCategory.vue
+++ src/view/ItemCategory.vue
@@ -372,7 +372,19 @@
 
       // 批量删除项目
       allDelete() {
-
+        let ids = []
+        this.multipleSelection.filter(v => {
+          ids.push(v.id)
+        })
+        this.$post('/project/batchDelete',{
+          ids:ids.join(',')
+        }).then(res=>{
+          this.$message({
+            message: '批量删除成功',
+            type: 'success'
+          })
+          this.getDataList()
+        })
       },
 
       // 上传导入文件
