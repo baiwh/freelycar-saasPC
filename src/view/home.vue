53cb8b360edc5eb80b0eedf5743d16f1ea0853b5
baiwh <471974983@qq.com>
2019-02-20 14:43:32 +0800

    首页

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>home</div>
+  <div>
+    <img class="page-center" src="./../../static/logo.jpg" alt="logo">
+    <div class="page-center">欢迎使用小易爱车门店管理系统</div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +10,14 @@
     name: 'home'
   }
 </script>
+<style lang="less" scoped>
+  .page-center{
+    transform: translateX(-50%);
+    left: 45%;
+    position: relative;
+    width: 300px;
+    text-align: center;
+    margin: 50px 0;
+  }
+</style>
 
