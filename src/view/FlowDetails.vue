88c3da1be0aee006e0cf1ccf4c38ac267eac292b
baiwh <471974983@qq.com>
2019-03-28 15:17:57 +0800

    流水明细

--- src/view/FlowDetails.vue
+++ src/view/FlowDetails.vue
@@ -14,37 +14,38 @@
         </el-date-picker>
       </el-col>
       <el-col :span="4">
-        <el-button type="primary" size="small">查询</el-button>
+        <el-button type="primary" size="small" @click="getData">查询</el-button>
       </el-col>
       <el-col :span="4">
-        <el-button type="primary" size="small">导出Excel</el-button>
+        <el-button type="primary" size="small" @click="getExcel">导出Excel</el-button>
       </el-col>
     </el-row>
 
     <!--合计金额-->
     <el-row>
       <el-col>
-        <span>合计金额：</span><el-input :value="sumAmounts" size="small" disabled></el-input>
+        <span>合计金额：</span>
+        <el-input :value="sumAmounts" size="small" disabled></el-input>
       </el-col>
 
     </el-row>
 
     <el-table v-loading="loading" :data="flowDetailsTable" border>
       <el-table-column type="index" label="序号" align="center"></el-table-column>
-      <el-table-column property="carModel" label="车型" align="center"></el-table-column>
-      <el-table-column property="carNumber" label="车牌号码" align="center"></el-table-column>
-      <el-table-column property="ownerName" label="车主姓名" align="center"></el-table-column>
-      <el-table-column property="ownerPhone" label="联系方式" align="center"></el-table-column>
-      <el-table-column property="ownerProject" label="消费项目" align="center"></el-table-column>
-      <el-table-column property="ownerCost" label="金额" align="center"></el-table-column>
-      <el-table-column property="costTime" label="时间" align="center"></el-table-column>
-      <el-table-column property="isVip" label="是否是会员" align="center"></el-table-column>
+      <el-table-column property="carBrand" label="车型" align="center"></el-table-column>
+      <el-table-column property="licensePlate" label="车牌号码" align="center"></el-table-column>
+      <el-table-column property="clientName" label="车主姓名" align="center"></el-table-column>
+      <el-table-column property="phone" label="联系方式" align="center"></el-table-column>
+      <el-table-column property="projectNames" label="消费项目" align="center"></el-table-column>
+      <el-table-column property="cost" label="金额" align="center"></el-table-column>
+      <el-table-column property="serviceTime" label="时间" align="center"></el-table-column>
+      <el-table-column property="isMember" label="是否是会员" align="center"></el-table-column>
     </el-table>
 
     <!--分页器-->
     <pagingDevice
       :pageData.sync="pageData"
-      @changePage="changePage"></pagingDevice>
+      @changePage="getData"></pagingDevice>
 
   </div>
 </template>
@@ -57,16 +58,7 @@
         loading: true,
         datePickerValue: '',
         sumAmounts: '',
-        flowDetailsTable: [{
-          carModel: '111',
-          carNumber: '222',
-          ownerName: 'xiaoyi',
-          ownerPhone: '123456789',
-          ownerProject: 'hahh',
-          ownerCost: '56',
-          costTime: '1234',
-          isVip: '是'
-        }],
+        flowDetailsTable: [],
         pageData: {
           currentPage: 1,
           pageSize: 10,
@@ -75,24 +67,49 @@
       }
     },
     methods: {
-      changePage() {
+      getData() {
+        this.$get('/order/listOrderParticulars', {
+          currentPage: this.pageData.currentPage,
+          pageSize: this.pageData.pageSize,
+          storeId: 1,
+          startTime: this.datePickerValue ? this.datePickerValue[0] : '',
+          endTime: this.datePickerValue ? this.datePickerValue[1] : ''
+        }).then(res => {
+          this.loading = false
+          this.sumAmounts = res.total
+          this.flowDetailsTable = res.data
+          this.pageData.currentPage = res.currentPage
+          this.pageData.pageSize = res.pageSize
+          this.pageData.pageTotal = res.total
+        })
+      },
 
+      getExcel() {
+        this.$getExcel('/order/exportOrderParticularsExcel', {
+          currentPage: this.pageData.currentPage,
+          pageSize: this.pageData.pageSize,
+          storeId: 1,
+          startTime: this.datePickerValue ? this.datePickerValue[0] : '',
+          endTime: this.datePickerValue ? this.datePickerValue[1] : ''
+        })
       }
     },
     mounted: function () {
-
+      this.getData()
     }
   }
 </script>
 
 <style lang="less" scoped>
-.el-date-picker {
-  width: 70%;
-}
-.el-row {
-  margin-bottom: 40px;
-}
-.el-input {
-  width: 100px;
-}
+  .el-date-picker {
+    width: 70%;
+  }
+
+  .el-row {
+    margin-bottom: 40px;
+  }
+
+  .el-input {
+    width: 100px;
+  }
 </style>
