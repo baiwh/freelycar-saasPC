6b76ed2758486f7cb48c2be3bd255ce3c5b9c173
tangtang233 <toby911115@163.com>
2019-07-10 10:58:27 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+# set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f Administrator@freelycar.com:/cygdrive/c/nginx/html/saas/
+
+cd -
